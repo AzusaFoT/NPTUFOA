ce72c409462d88a57fbbbdcd96c1a4be294eb3ad
Azusa <fot531@gmail.com>
2022-05-20 16:23:01 +0800

    <feat>layout

---
+++ NPTUFOA/webViewController.swift
@@ -0,0 +1,24 @@
+//
+//  webViewController.swift
+//  NPTUFOA
+//
+//  Created by students on 2022/5/20.
+//
+
+import UIKit
+import WebKit
+
+class webViewController: UIViewController {
+
+    @IBAction func backButton(_ sender: UIButton) {
+        
+    }
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
