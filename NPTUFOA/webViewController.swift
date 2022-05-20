5e62bb9cb53a9e96edfb320cc8ecb5b6235337d7
xinnn <chen256ella@gmail.com>
2022-05-20 17:36:02 +0800

    <feat>website

--- NPTUFOA/webViewController.swift
+++ NPTUFOA/webViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 import WebKit
 
-class webViewController: UIViewController {
+class webViewController: UIViewController,WKUIDelegate,WKNavigationDelegate {
 
+    
     @IBAction func backButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     @IBOutlet weak var webView: WKWebView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let myURL = URL(string:"https://sheetdb.io/s/f/sboj9o8hufsrq")
+                let myRequest = URLRequest(url: myURL!)
+                webView.load(myRequest)
     }
     
+    
 
+    
 }
